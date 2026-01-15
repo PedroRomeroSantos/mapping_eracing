fac42333728189c539f12278913317e3ff25828f
Pedro Romero Santos <galaga100prs@gmail.com>
2026-01-14 22:57:34 -0300

    Update tradução_controle.py

--- tradução_controle.py
+++ tradução_controle.py
@@ -4,19 +4,18 @@ from std_msgs.msg import Float32MultiArray
 from fs_msgs.msg import ControlCommand
 
 def callback(msg):
-    # Verificamos se o array tem os 3 valores [steering, throttle, brake]
+    #verifica 3 valores [steering, throttle, brake]
     if len(msg.data) >= 2:
         cmd = ControlCommand()
-        # Header é importante em algumas versões do FSDS
         cmd.header.stamp = rospy.Time.now()
         
-        # Mapeamento correto dos índices
+        # índices corretos
         cmd.steering = float(msg.data[0])
         cmd.throttle = float(msg.data[1])
         cmd.brake = 0.0 # Force o freio em zero absoluto
         
         pub.publish(cmd)
-        # Log para conferir no terminal do ROS 1
+        #conferir no terminal do ROS 1
         # rospy.loginfo("T: %f | S: %f", cmd.throttle, cmd.steering)
 
 rospy.init_node('bridge_translator')
